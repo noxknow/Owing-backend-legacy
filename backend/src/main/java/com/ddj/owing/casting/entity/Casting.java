9e6c28691c55f3016bd1109c9729dc7ad387e85c
chris2769 <chris2769@naver.com>
2024-09-24 11:02:19 +0900

    #11 [feat] : Casting 클래스 BasetimeEntity 상속 구현 추가

--- backend/src/main/java/com/ddj/owing/casting/entity/Casting.java
+++ backend/src/main/java/com/ddj/owing/casting/entity/Casting.java
@@ -1,5 +1,6 @@
 package com.ddj.owing.casting.entity;
 
+import com.ddj.owing.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -8,7 +9,7 @@ import lombok.*;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Casting {
+public class Casting extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
