d23c9398d45eab51772f749d9bde549c39601df5
chris2769 <chris2769@naver.com>
2024-09-24 17:01:20 +0900

    #15 [feat] : 세계관 이미지 생성 Controller 구현

--- backend/src/main/java/com/ddj/owing/domain/universe/controller/UniverseController.java
+++ backend/src/main/java/com/ddj/owing/domain/universe/controller/UniverseController.java
@@ -1,6 +1,11 @@
 package com.ddj.owing.domain.universe.controller;
 
+import com.ddj.owing.domain.universe.model.dto.UniverseRequestDto;
+import com.ddj.owing.domain.universe.service.UniverseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class UniverseController {
 
+    private final UniverseService universeService;
 
+    @PostMapping("/universe/generate")
+    public ResponseEntity<String> generateUniverseImage(@RequestBody UniverseRequestDto universeRequestDto) {
+        return universeService.generateUniverseImage(universeRequestDto);
+    }
 }
