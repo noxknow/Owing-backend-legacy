49e2e33ec5cddde8dfbf1fdd9d9924cdcffcefff
chris2769 <chris2769@naver.com>
2024-09-25 16:45:15 +0900

    #18 [feat] : 프로젝트 정보 보내는 dto 생성

---
+++ backend/src/main/java/com/ddj/owing/domain/project/model/dto/ProjectInfoResponseDto.java
@@ -0,0 +1,20 @@
+package com.ddj.owing.domain.project.model.dto;
+
+import com.ddj.owing.domain.project.model.Project;
+
+import java.time.LocalDateTime;
+
+public record ProjectInfoResponseDto(
+        String title,
+        LocalDateTime updatedAt,
+        String coverImage
+) {
+
+    public static ProjectInfoResponseDto from(Project project) {
+        return new ProjectInfoResponseDto(
+                project.getTitle(),
+                project.getUpdatedAt(),
+                project.getCoverImage()
+        );
+    }
+}
