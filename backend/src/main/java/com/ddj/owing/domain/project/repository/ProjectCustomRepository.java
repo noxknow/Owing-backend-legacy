bb14fb9c8c2a615db46bd5a97edd2d059a0ba530
chris2769 <chris2769@naver.com>
2024-09-25 18:18:45 +0900

    #18 [feat] : QueryDSL 설정 및 커스텀 레포지토리 생성

---
+++ backend/src/main/java/com/ddj/owing/domain/project/repository/ProjectCustomRepository.java
@@ -0,0 +1,10 @@
+package com.ddj.owing.domain.project.repository;
+
+import com.ddj.owing.domain.project.model.Project;
+
+import java.util.List;
+
+public interface ProjectCustomRepository {
+
+    List<Project> findTop3ByOrderByUpdatedAtDesc();
+}
