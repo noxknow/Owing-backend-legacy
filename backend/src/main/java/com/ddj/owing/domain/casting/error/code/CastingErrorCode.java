ed6f6cf45999ee59ef550bdd375b336d6663949d
given-dragon <yijy001@gmail.com>
2024-09-26 16:28:48 +0900

    #22 [feat] : Casting error 추가

---
+++ backend/src/main/java/com/ddj/owing/domain/casting/error/code/CastingErrorCode.java
@@ -0,0 +1,20 @@
+package com.ddj.owing.domain.casting.error.code;
+
+import com.ddj.owing.global.error.code.OwingErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum CastingErrorCode implements OwingErrorCode {
+	CASTING_NOT_FOUND(HttpStatus.NOT_FOUND, "001", "캐릭터를 찾을 수 없습니다.");
+
+	private final HttpStatus status;
+	private final String code;
+	private final String message;
+
+	CastingErrorCode(HttpStatus status, String code, String message) {
+		this.status = status;
+		this.code = "CASTING" + code;
+		this.message = message;
+	}
+}
