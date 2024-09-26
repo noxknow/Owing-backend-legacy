6b472b5435094b8d333ae36ec210d54977246de5
given-dragon <yijy001@gmail.com>
2024-09-26 16:27:32 +0900

    #22 [feat] : Casting Graph Entity, Repository 추가

---
+++ backend/src/main/java/com/ddj/owing/domain/casting/model/CastingNode.java
@@ -0,0 +1,31 @@
+package com.ddj.owing.domain.casting.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.hibernate.annotations.SoftDelete;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+@Getter
+@Node("Cast")
+public class CastingNode {
+    @Id
+    private Long id;
+    private String name;
+    private Long age;
+    private String gender;
+    private String role;
+    private String detail;
+    private String imageUrl;
+
+    @Builder
+    public CastingNode(Long id, String name, Long age, String gender, String role, String detail, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.role = role;
+        this.detail = detail;
+        this.imageUrl = imageUrl;
+    }
+}
