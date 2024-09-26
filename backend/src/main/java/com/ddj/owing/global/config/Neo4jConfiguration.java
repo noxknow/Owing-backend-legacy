08b5d86897e891b5d39a276bc7985cad6ccd1a52
given-dragon <yijy001@gmail.com>
2024-09-26 16:26:40 +0900

    #22 [feat] : Neo4j Config 추가

---
+++ backend/src/main/java/com/ddj/owing/global/config/Neo4jConfiguration.java
@@ -0,0 +1,24 @@
+package com.ddj.owing.global.config;
+
+import org.neo4j.cypherdsl.core.renderer.Dialect;
+import org.neo4j.driver.Driver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.core.transaction.Neo4jTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class Neo4jConfiguration {
+
+    @Bean
+    protected org.neo4j.cypherdsl.core.renderer.Configuration cypherDslConfiguration() {
+        return org.neo4j.cypherdsl.core.renderer.Configuration.newConfig().withDialect(Dialect.NEO4J_5).build();
+    }
+
+    @Bean
+    protected PlatformTransactionManager transactionManager(Driver driver) {
+        return new Neo4jTransactionManager(driver);
+    }
+}
