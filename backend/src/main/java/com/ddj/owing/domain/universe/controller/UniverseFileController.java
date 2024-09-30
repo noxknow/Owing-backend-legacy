8f034bb62b919a48412c20e6c7a882e6e11692c7
chris2769 <chris2769@naver.com>
2024-09-30 10:34:14 +0900

    #23 [feat] : 세계관 상세 정보 불러오는 컨트롤러 구현

---
+++ backend/src/main/java/com/ddj/owing/domain/universe/controller/UniverseFileController.java
@@ -0,0 +1,26 @@
+package com.ddj.owing.domain.universe.controller;
+
+import com.ddj.owing.domain.universe.model.UniverseFile;
+import com.ddj.owing.domain.universe.model.dto.UniverseFileRequestDto;
+import com.ddj.owing.domain.universe.service.UniverseFileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/universe")
+public class UniverseFileController {
+
+    private final UniverseFileService universeFileService;
+
+    @GetMapping("/info/{id}")
+    public ResponseEntity<UniverseFile> loadUniverseInfo(@PathVariable("id") Long id) {
+        return universeFileService.loadUniverseInfo(id);
+    }
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateUniverseImage(@RequestBody UniverseFileRequestDto universeFileRequestDto) {
+        return universeFileService.generateUniverseImage(universeFileRequestDto);
+    }
+}
