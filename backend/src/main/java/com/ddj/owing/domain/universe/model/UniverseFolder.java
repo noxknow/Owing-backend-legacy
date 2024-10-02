4bfc9ad6cf6061b74aaea5986d89ec6f870f3a7b
chris2769 <chris2769@naver.com>
2024-10-02 16:58:28 +0900

    #23 [feat] : 폴더 내용 없데이트 위한 Entity 메서드 추가

--- backend/src/main/java/com/ddj/owing/domain/universe/model/UniverseFolder.java
+++ backend/src/main/java/com/ddj/owing/domain/universe/model/UniverseFolder.java
@@ -1,5 +1,6 @@
 package com.ddj.owing.domain.universe.model;
 
+import com.ddj.owing.domain.universe.model.dto.UniverseFolderUpdateRequestDto;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.SoftDelete;
@@ -30,4 +31,9 @@ public class UniverseFolder {
 
     @OneToMany(mappedBy = "universeFolder", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<UniverseFile> universeFiles = new ArrayList<>();
+
+    public void updateFolder(UniverseFolderUpdateRequestDto universeFolderUpdateRequestDto) {
+        this.title = universeFolderUpdateRequestDto.title();
+        this.description = universeFolderUpdateRequestDto.description();
+    }
 }
