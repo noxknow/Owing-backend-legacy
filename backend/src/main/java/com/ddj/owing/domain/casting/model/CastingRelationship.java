2f59c1157439f6dbd3c21b933e1a4dfd22b9ccfb
given-dragon <yijy001@gmail.com>
2024-10-02 16:13:15 +0900

    #31 [fix] : reflection을 위한 기본생성자 추가

--- backend/src/main/java/com/ddj/owing/domain/casting/model/CastingRelationship.java
+++ backend/src/main/java/com/ddj/owing/domain/casting/model/CastingRelationship.java
@@ -3,7 +3,9 @@ package com.ddj.owing.domain.casting.model;
 import com.ddj.owing.domain.casting.error.code.CastingErrorCode;
 import com.ddj.owing.domain.casting.error.exception.CastingException;
 import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.neo4j.core.schema.RelationshipId;
 import org.springframework.data.neo4j.core.schema.RelationshipProperties;
 import org.springframework.data.neo4j.core.schema.TargetNode;
@@ -11,6 +13,7 @@ import org.springframework.util.StringUtils;
 
 @RelationshipProperties
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CastingRelationship {
     @RelationshipId
     private Long id;
