d14f9920237424579ed76f7c583768edc847b27e
given_dragon <yijy001@naver.com>
2024-10-04 17:21:41 +0900

    #35 [feat] : StoryPlotNode, Repository 추가

---
+++ backend/src/main/java/com/ddj/owing/domain/story/model/StoryPlotNode.java
@@ -0,0 +1,21 @@
+package com.ddj.owing.domain.story.model;
+
+
+import com.ddj.owing.global.entity.BaseTimeGraph;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+@Node("StoryPlot")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StoryPlotNode extends BaseTimeGraph {
+	@Id
+	private Long id;
+
+	private String name;
+}
---
+++ backend/src/main/java/com/ddj/owing/domain/story/repository/StoryPlotNodeRepository.java
@@ -0,0 +1,9 @@
+package com.ddj.owing.domain.story.repository;
+
+import com.ddj.owing.domain.story.model.StoryPlotNode;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface StoryPlotNodeRepository extends Neo4jRepository<StoryPlotNode, Long> {
+}
