90634e97420e72d9a02d7dc2322f2f9cc5a65065
given-dragon <yijy001@gmail.com>
2024-10-04 23:22:48 +0900

    #35 [feat] : StoryPlotNodeRepository.findById 쿼리 추가

--- backend/src/main/java/com/ddj/owing/domain/story/repository/StoryPlotNodeRepository.java
+++ backend/src/main/java/com/ddj/owing/domain/story/repository/StoryPlotNodeRepository.java
@@ -2,8 +2,14 @@ package com.ddj.owing.domain.story.repository;
 
 import com.ddj.owing.domain.story.model.StoryPlotNode;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StoryPlotNodeRepository extends Neo4jRepository<StoryPlotNode, Long> {
+
+    @Query("MATCH (n:StoryPlot{id: $id}) WHERE n.deletedAt IS NULL return n")
+    Optional<StoryPlotNode> findById(Long id);
 }
