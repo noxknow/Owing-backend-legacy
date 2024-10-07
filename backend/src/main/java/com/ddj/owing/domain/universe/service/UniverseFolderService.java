05a27222954cc8956f233c0a72c07d5ef4d17361
chris2769 <chris2769@naver.com>
2024-10-07 17:28:44 +0900

    #38 [feat] : 폴더 리스트 반환하는 로직 수정

--- backend/src/main/java/com/ddj/owing/domain/universe/service/UniverseFolderService.java
+++ backend/src/main/java/com/ddj/owing/domain/universe/service/UniverseFolderService.java
@@ -3,7 +3,7 @@ package com.ddj.owing.domain.universe.service;
 import com.ddj.owing.domain.universe.error.code.UniverseFolderErrorCode;
 import com.ddj.owing.domain.universe.error.exception.UniverseFolderException;
 import com.ddj.owing.domain.universe.model.UniverseFolder;
-import com.ddj.owing.domain.universe.model.dto.UniverseFolderCreateDto;
+import com.ddj.owing.domain.universe.model.dto.UniverseFolderCreateRequestDto;
 import com.ddj.owing.domain.universe.model.dto.UniverseFolderResponseDto;
 import com.ddj.owing.domain.universe.model.dto.UniverseFolderUpdateRequestDto;
 import com.ddj.owing.domain.universe.repository.UniverseFolderRepository;
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,13 +24,13 @@ public class UniverseFolderService {
     /**
      * 새로운 폴더를 생성하는 메서드
      *
-     * @param universeFolderCreateDto 폴더 생성 요청에 필요한 정보를 담은 DTO
+     * @param universeFolderCreateRequestDto 폴더 생성 요청에 필요한 정보를 담은 DTO
      * @return 생성 성공 시 빈 ResponseEntity 반환
      */
     @Transactional
-    public ResponseEntity<Void> createFolder(UniverseFolderCreateDto universeFolderCreateDto) {
+    public ResponseEntity<Void> createFolder(UniverseFolderCreateRequestDto universeFolderCreateRequestDto) {
 
-        UniverseFolder universeFolder = universeFolderCreateDto.toEntity();
+        UniverseFolder universeFolder = universeFolderCreateRequestDto.toEntity();
         universeFolderRepository.save(universeFolder);
 
         return ResponseEntity.ok().build();
@@ -41,26 +42,31 @@ public class UniverseFolderService {
      * @return UniverseFolder 리스트를 ResponseEntity 로 감싸서 반환
      */
     @Transactional(readOnly = true)
-    public ResponseEntity<List<UniverseFolder>> getAllFolders() {
+    public ResponseEntity<List<UniverseFolderResponseDto>> getAllFolders() {
 
-        return ResponseEntity.ok(universeFolderRepository.findAll());
+        List<UniverseFolder> universeFolders = universeFolderRepository.findAll();
+        List<UniverseFolderResponseDto> universeFolderResponseDtoList = universeFolders.stream()
+                .map(UniverseFolderResponseDto::fromEntity)
+                .toList();
+
+        return ResponseEntity.ok(universeFolderResponseDtoList);
     }
 
-    /**
-     * ID로 특정 폴더를 조회하는 메서드 (읽기 전용)
-     *
-     * @param id 조회할 폴더의 고유 ID
-     * @return 조회된 폴더를 ResponseEntity 로 감싸서 반환
-     */
-    @Transactional(readOnly = true)
-    public ResponseEntity<UniverseFolderResponseDto> getFolderById(Long id) {
-
-        UniverseFolder universeFolder = universeFolderRepository.findById(id)
-                .orElseThrow(() -> UniverseFolderException.of(UniverseFolderErrorCode.UNIVERSE_FOLDER_NOT_FOUND));
-        UniverseFolderResponseDto universeFolderResponseDto = UniverseFolderResponseDto.fromEntity(universeFolder);
-
-        return ResponseEntity.ok(universeFolderResponseDto);
-    }
+//    /**
+//     * ID로 특정 폴더를 조회하는 메서드 (읽기 전용)
+//     *
+//     * @param id 조회할 폴더의 고유 ID
+//     * @return 조회된 폴더를 ResponseEntity 로 감싸서 반환
+//     */
+//    @Transactional(readOnly = true)
+//    public ResponseEntity<UniverseFolderResponseDto> getFolderById(Long id) {
+//
+//        UniverseFolder universeFolder = universeFolderRepository.findById(id)
+//                .orElseThrow(() -> UniverseFolderException.of(UniverseFolderErrorCode.UNIVERSE_FOLDER_NOT_FOUND));
+//        UniverseFolderResponseDto universeFolderResponseDto = UniverseFolderResponseDto.fromEntity(universeFolder);
+//
+//        return ResponseEntity.ok(universeFolderResponseDto);
+//    }
 
     /**
      * 폴더를 삭제하는 메서드
@@ -81,8 +87,8 @@ public class UniverseFolderService {
     /**
      * 폴더 정보를 업데이트하는 메서드
      *
-     * @param id                         업데이트할 폴더의 고유 ID
-     * @param universeFolderUpdateRequestDto    업데이트 요청에 필요한 정보를 담은 DTO
+     * @param id 업데이트할 폴더의 고유 ID
+     * @param universeFolderUpdateRequestDto 업데이트 요청에 필요한 정보를 담은 DTO
      * @return 업데이트 성공 시 빈 ResponseEntity 반환
      */
     @Transactional
