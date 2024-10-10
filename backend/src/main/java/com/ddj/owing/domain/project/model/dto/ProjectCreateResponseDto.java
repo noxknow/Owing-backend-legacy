811230e1bc7e2241ba5cee84746851ae35e0cd76
chris2769 <chris2769@naver.com>
2024-10-10 14:28:43 +0900

    #54 [feat] : dto 필드값 변경

--- backend/src/main/java/com/ddj/owing/domain/project/model/dto/ProjectCreateResponseDto.java
+++ backend/src/main/java/com/ddj/owing/domain/project/model/dto/ProjectCreateResponseDto.java
@@ -1,6 +1,21 @@
 package com.ddj.owing.domain.project.model.dto;
 
+import com.ddj.owing.domain.project.model.Project;
+import lombok.Builder;
+
+@Builder
 public record ProjectCreateResponseDto(
-        Long id
+        Long id,
+        String title,
+        String imageUrl
 ) {
+
+    public static ProjectCreateResponseDto fromEntity (Project project, String presignedUrl) {
+
+        return ProjectCreateResponseDto.builder()
+                .id(project.getId())
+                .title(project.getTitle())
+                .imageUrl(presignedUrl)
+                .build();
+    }
 }
