848e8fddff6088d9f8bd9f9a7613ce9a04f60c70
han <chos9715@naver.com>
2024-10-11 09:34:32 +0900

    #67 [hotfix] : 세계관 폴더 생성시 projectId 추가

--- backend/src/main/java/com/ddj/owing/domain/universe/model/dto/UniverseFolderCreateRequestDto.java
+++ backend/src/main/java/com/ddj/owing/domain/universe/model/dto/UniverseFolderCreateRequestDto.java
@@ -3,15 +3,17 @@ package com.ddj.owing.domain.universe.model.dto;
 import com.ddj.owing.domain.universe.model.UniverseFolder;
 
 public record UniverseFolderCreateRequestDto(
-        String name,
-        String description
+	String name,
+	String description,
+	Long projectId
 ) {
 
-    public UniverseFolder toEntity() {
+	public UniverseFolder toEntity() {
 
-        return UniverseFolder.builder()
-                .name(name)
-                .description(description)
-                .build();
-    }
+		return UniverseFolder.builder()
+			.name(name)
+			.description(description)
+			.projectId(projectId)
+			.build();
+	}
 }
